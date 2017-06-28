86c9cf29f68ecaf2a1cdd39a0c8f72b64ca0c497
Hermann Krumrey <hermann@krumreyh.com>
2017-06-28 20:20:40 +0200

    Copy all instructions from drr_demo

--- octo-apps/drr_demo/src/drr_demo.rs
+++ octo-apps/drr_demo/src/drr_demo.rs
@@ -32,11 +32,50 @@ pub extern "C" fn main_rust_ilet(claim: u8) {
     // Now let's invade three cores on tile 1.
 	// The proxy_invade() call works asynchronously - we can continue doing
 	// stuff while the request is being processed.
-	let future: invade_future;
-	if (proxy_invade(1, &future, 3) != 0) {
-		fputs("proxy_invade failed - does tile 1 even exist?\n", stderr);
-		abort();
-	}
+	// TODO implement invade_future, proxy_invade, abort
+	// let future: invade_future;
+	// if proxy_invade(1, &future, 3) != 0 {
+	//     print_text("proxy_invade failed - does tile 1 even exist?\n\0");
+	//	   abort();
+	// }
+
+	// In theory, we could do arbitrary stuff between proxy_invade() and
+	// invade_future_force(). In this sample application, we don't.
+
+	// Now let's go see if the invasion worked. If it did, we get a proxy claim
+	// to which we can send i-lets to be executed on tile 1.
+	// TODO Implement proxy_claim_t, invade_future_force
+	// let pc: proxy_claim_t = invade_future_force(&future);
+	// if pc == 0 {
+	//	   print_text("invade_future_force failed - looks like 3 free cores are \
+	//	               not available on tile 1.\n");
+	//	   abort();
+	// }
+
+	// TODO BOOKMARK
+
+	// We will send an i-let to tile 1 and then want to wait until that i-let
+	// has finished executing. For this purpose, we use a signalling primitive.
+	// The simple_signal construct holds an internal counter and offers two
+	// operations:
+	// * simple_signal_wait() suspends the calling i-let until the counter
+	//   reaches zero. If the counter is already zero, it returns immediately.
+	// * simple_signal_signal() decrements the counter. If the counter reaches
+	//   zero, the suspended i-let is woken up.
+
+	// Create a signal and initialise its counter with 1.
+	// Note that the signal lies on the i-let's stack, in our own TLM.
+	simple_signal signal;
+	simple_signal_init(&signal, 1);
+
+	// Create the i-let we want to execute on tile 1, and pass a pointer to the
+	// signal as argument.
+	simple_ilet iLet;
+	simple_ilet_init(&iLet, remoteILetFunc, &signal);
+	proxy_infect(pc, &iLet, 1);
+
+	// Now wait until the remote i-let has notified us of its completion.
+	simple_signal_wait(&signal);
 
     // Shut down the system.
 	// If we don't do this, the system will keep running forever. Simply
