7792ba5b1824e0b7249685f51fa01c917cd6221d
Hermann Krumrey <hermann@krumreyh.com>
2017-07-03 01:23:09 +0200

    Start attempt of porting octo_agent.h

--- octolib/src/octo_bindings/octo_types.rs
+++ octolib/src/octo_bindings/octo_types.rs
@@ -1,3 +1,11 @@
+use libc::c_void;
+
 pub type claim_t = u8;
 pub type tile_id_t = u8;
+pub type agent_t = * c_void;
+pub type agentclaim_t = * c_void;
+pub type constraints_t = * c_void;
 // TODO continue
+
+res_type_t
+proxy_claim_t
