7d0d4730f14f9f032a78047dbab96378cc243fa7
Hermann Krumrey <hermann@krumreyh.com>
2017-07-10 11:26:02 +0200

    Generate bindings for octo_claim.

--- octolib/src/lib.rs
+++ octolib/src/lib.rs
@@ -25,6 +25,7 @@ pub mod octo_dispatch_claim;
 pub mod octo_leon;
 pub mod octo_app;
 pub mod octo_cilk_support;
+pub mod octo_claim;
 
 // Usually in std, must be defined for an executable file
 #[lang = "eh_personality"] extern fn eh_personality() {}
