22270111044d40dc5e817da35d61b0c49190d09e
Hermann Krumrey <hermann@krumreyh.com>
2017-09-17 22:11:45 +0200

    More ways to use user data

--- octo-apps/experiments/user_parameters/src/lib.rs
+++ octo-apps/experiments/user_parameters/src/lib.rs
@@ -48,4 +48,29 @@ pub extern "C" fn rust_main_ilet(claim: u8) {
 
 	claim.infect(sum, Some(&arrays));
 
+	let mut more_arrays = [
+		[1,1,1,1,1, -1],
+		[2,2,2,2,2, -1],
+		[3,3,3,3,3, -1],
+		[4,4,4,4,4, -1]
+	];
+	let indices = [0 as *mut c_void, 1 as *mut c_void, 2 as *mut c_void, 3 as *mut c_void];
+	let mut total_sum = 0;
+
+	claim.infect(|params: *mut c_void| {
+		let slice = more_arrays[params as usize];
+		let sum = slice[0] + slice[1] + slice[2] + slice[3] + slice[4];
+		more_arrays[params as usize][5] = sum;
+		total_sum += sum;
+	}, Some(&indices));
+
+	for i in 0..4 {
+		unsafe {
+			printf("Sum %d: %d\n\0".as_ptr(), i, more_arrays[i][5]);
+		}
+	}
+	unsafe {
+		printf("Total: %d\n\0".as_ptr(), total_sum);
+	}
+
 }
