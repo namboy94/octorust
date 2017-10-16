9b4aaf6a2cfcfac79e426dc0f70393dcf8fd4c3b
Hermann Krumrey <hermann@krumreyh.com>
2017-10-16 22:48:51 +0200

    Add compile time comparisons

--- eval/benchmarks/run.py
+++ eval/benchmarks/run.py
@@ -1,13 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import sys
 import time
+import shutil
 from subprocess import Popen
 
 # constants
 RUN_COUNT = 1 if len(sys.argv) == 1 else int(sys.argv[1])
+ENABLE_X10 = False
+RECOMPILE_RUST = False
 
+
+def colour_lang(l):
+
+    colour = {
+        "Rust": "\033[1;33m",
+        "C": "\033[1;31m",
+        "X10": "\033[1;35m"
+    }[l]
+    return colour + l + "\033[0;0m"
 
 if __name__ == "__main__":
 
@@ -26,7 +38,7 @@ if __name__ == "__main__":
 
     for eval_dir in [
         "startup",
-        "primes-naive"
+        "primes-naive",
         # "primes-eratosthenes",
         # "garbageonly-gc-benchmark"
     ]:
@@ -34,6 +46,7 @@ if __name__ == "__main__":
 
         runtimes = {}
         sizes = {}
+        compile_times = {}
 
         if os.path.isdir(eval_dir_path):
 
@@ -43,16 +56,32 @@ if __name__ == "__main__":
                 lang = "generic"
 
                 if test.endswith(".c") or os.path.isdir(test_file):
+
+                    if os.path.isdir(test_file) and RECOMPILE_RUST:
+                        # Remove pre-compiled stuff to make compile time
+                        # comparisons fair
+                        target_dir = os.path.join(test_file, "target")
+                        if os.path.isdir(target_dir):
+                            shutil.rmtree(target_dir)
+
                     lang = "C" if test.endswith(".c") else "Rust"
+                    compile_start_time = time.time()
                     Popen(octorust_base_cmd + [test_file]).wait()
+                    compile_times[lang] = time.time() - compile_start_time
 
                 elif test.endswith(".x10"):
+
+                    if not ENABLE_X10:
+                        continue
+
                     lang = "X10"
+                    compile_start_time = time.time()
                     Popen([
                         "x10firm", "-v",
                         "-mtarget=i686-invasic-irtss",
                         test_file, "-o", "out"
                     ]).wait()
+                    compile_times[lang] = time.time() - compile_start_time
 
                 else:
                     continue
@@ -68,26 +97,31 @@ if __name__ == "__main__":
                 sizes[lang] = os.path.getsize("out")
                 os.remove("out")
 
-        bench_data[eval_dir] = (runtimes, sizes)
+        bench_data[eval_dir] = (runtimes, sizes, compile_times)
 
     for data in bench_data.keys():
         runtimes = bench_data[data][0]
         sizes = bench_data[data][1]
+        compile_times = bench_data[data][2]
 
         print("\033[1;36m" + data + "\033[0;0m:\n")
+
+        print("\033[1;34mCompile Times\033[0;0m:")
+        for lang in sorted(compile_times, key=compile_times.get):
+            compile_time = compile_times[lang]
+            # noinspection PyUnresolvedReferences
+            print(colour_lang(lang).ljust(22) + str(round(compile_time, 4)))
 
         print("\033[1;34mRuntimes\033[0;0m:")
         for lang in sorted(runtimes, key=runtimes.get):
             runtime = runtimes[lang]
             # noinspection PyUnresolvedReferences
-            print(lang.ljust(15) + str(round(runtime, 4)))
+            print(colour_lang(lang).ljust(22) + str(round(runtime, 4)))
 
         print("\033[1;34mFile Sizes\033[0;0m:")
         for lang in sorted(sizes, key=sizes.get):
             filesize = sizes[lang]
             # noinspection PyUnresolvedReferences
-            print(lang.ljust(15) + str(filesize))
+            print(colour_lang(lang).ljust(22) + str(filesize))
 
-        print("--------------------------------------------------------------")
-
-
+        print("-------------------------------------------------------------")
