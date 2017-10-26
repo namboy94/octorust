df7e38b11c61a0a0c74305d48f2d45a94f1794e0
Hermann Krumrey <hermann@krumreyh.com>
2017-10-26 18:32:41 +0200

    Fix compile_time.sh x10firm commands

--- eval/benchmarks/compile_times.sh
+++ eval/benchmarks/compile_times.sh
@@ -4,5 +4,5 @@ temci short exec -wd "octorust -i 2017-06-07 startup/startup -o out" --runs 50 -
 temci short exec -wd "octorust -i 2017-06-07 startup/startup --release -o out" --runs 50 --out temci_output/compile_rust_opt.yaml
 temci short exec -wd "octorust -i 2017-06-07 startup/startup.c -o out" --runs 50 --out temci_output/compile_c.yaml
 temci short exec -wd "octorust -i 2017-06-07 startup/startup.c --release -o out" --runs 50 --out temci_output/compile_c_opt.yaml
-temci short exec -wd "x10i startup/Startup.x10 -mtarget=i686-invasic-irtss -o out" --runs 50 --out temci_output/compile_x10.yaml
-temci short exec -wd "x10i startup/Startup.x10 -mtarget=i686-invasic-irtss -o out -O3" --runs 50 --out temci_output/compile_x10_opt.yaml
+temci short exec -wd "x10firm startup/Startup.x10 -mtarget=i686-invasic-irtss -o out" --runs 50 --out temci_output/compile_x10.yaml
+temci short exec -wd "x10firm startup/Startup.x10 -mtarget=i686-invasic-irtss -o out -O3" --runs 50 --out temci_output/compile_x10_opt.yaml
