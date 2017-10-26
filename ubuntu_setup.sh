832c8d7f4051a2c1c0f3a3a30273f10cf3a856a5
Hermann Krumrey <hermann@krumreyh.com>
2017-10-26 20:21:04 +0200

    Modify Ubuntu setup file

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 echo "Installing python 3 and setuptools"
-sudo apt install python3 python3-setuptools
+sudo apt install python3 python3-setuptools python3-pip
 
 echo "Installing gcc-multilib"
 sudo apt install gcc-multilib
+
+echo "Installing curl"
+sudo apt install curl
 
 echo "Installing rustup"
 curl https://sh.rustup.rs -sSf | sh
@@ -14,6 +17,7 @@ rustup default nightly-2017-06-01
 rustup target install i686-unknown-linux-gnu
 
 echo "Installing octorust"
+pip3 install toml --user
 python3 setup.py install --user
 echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 export PATH=$PATH:~/.local/bin
